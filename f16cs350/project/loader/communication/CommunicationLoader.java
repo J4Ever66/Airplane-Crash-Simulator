e80e83fe0fd41d8bf3f16e49127488332709e0b1
j4ever66 <jordanever14@gmail.com>
2016-11-16 15:24:34 -0800

    Added duplicate coordinate checking for TerrainLoader, cleaned up comments. Implemented minor methods(non encode/decode) in CommunicationLoader.

--- f16cs350/project/loader/communication/CommunicationLoader.java
+++ f16cs350/project/loader/communication/CommunicationLoader.java
@@ -9,22 +9,42 @@ import java.util.Map;
  *
  **/
 public class CommunicationLoader {
-    private int base, encodeSize;
+    private int base, encodeSize, checksum, wordCount;
+    private String[] dictionaryString = { "a", "above", "across", "affirmative", "after", "again", "ah", "airport", "alpha", "altitude", "american", "an", "and", "approach",
+            "approved", "are", "as", "at", "atis", "before", "begin", "behind", "below", "bravo", "center", "cessna", "charlie", "clearance", "cleared", "climb", "contact", "correct",
+            "course", "cross", "declare", "decrease", "degrees", "delay", "delivery", "delta", "departure", "descend", "direct", "discretion", "dme", "do", "east", "echo", "eight", "emergency",
+            "enable", "engine", "established", "execute", "executing", "expect", "failure", "feet", "filed", "fire", "five", "flight", "for", "four", "foxtrot", "frequency", "front", "fuel", "gate",
+            "golf", "has", "have", "heading", "heavy", "hold", "hotel", "how", "hundred", "ident", "if", "ils", "immediate", "inbound", "increase", "india", "intentions", "intercept", "is", "juliett",
+            "kilo", "land", "landing", "leaving", "left", "level", "lima", "looking", "maintain", "mayday", "mike", "minute", "minutes", "missed", "navigation", "ndb", "negative", "niner", "north",
+            "northeast", "northwest", "not", "november", "o'clock", "of", "one", "only", "option", "or", "oscar", "out", "outbound", "own", "pan-pan", "papa", "per", "pilot", "point", "problem", "proceed",
+            "quebec", "radar", "ramp", "readback", "ready", "report", "request", "resume", "right", "risk", "roger", "romeo", "runway", "say", "second", "seconds", "service", "seven", "sierra", "sight",
+            "six", "south", "southeast", "southwest", "speed", "squawk", "standby", "takeoff", "tango", "taxi", "taxiway", "terminal", "terminated", "terrain", "the", "then", "thousand", "three", "to",
+            "tower", "traffic", "turn", "two", "um", "unable", "uniform", "united", "until", "vector", "vfr", "via", "victor", "vor", "west", "what", "when", "where", "whether", "whiskey", "who", "why",
+            "wilco", "will", "with", "x-ray", "yankee", "you", "zero", "zulu"};
+
     private HashMap<String, String> dictionary;
     private HashSet<String> customWords;
     private String[] key = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e",
     "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-
-
     public CommunicationLoader(int base, int encodingSize){
+        this.wordCount = 0;
         this.base = base;
         this.encodeSize = encodingSize;
+        this.customWords = new HashSet<>();
+        this.dictionary = new HashMap<>();
+        fillDictionary();
+
+        this.checksum = dictionary.hashCode();
     }
 
 
     public int calculateMaxValue(int base, int size){
-        return -1;
+        double sum = 0;
+        for(int x = 0; x < size; x++){
+            sum += (base - 1) * Math.pow(base,  x);
+        }
+        return (int)sum;
     }
 
     public String decodeStatement(String s){
@@ -48,27 +68,69 @@ public class CommunicationLoader {
     }
 
     public String registerCustomWord(String index, String word){
-        return null;
+        this.dictionary.put(index, word);
+        this.checksum = dictionary.hashCode();
+        return index;
     }
 
     public boolean isValidCustomIndex(String index){
+        int indexVal = getIndex(index);
+        if(indexVal > wordCount && indexVal <= calculateMaxValue(base, encodeSize)){
+            return true;
+        }
         return false;
     }
 
-    public boolean isValidIndex(String index){
+    public boolean isValidIndex(String index){ // getMaxValue
+        int indexVal = getIndex(index);
+        if(indexVal >= 0 && indexVal <= calculateMaxValue(base, encodeSize)){
+            return true;
+        }
         return false;
+    }
+
+    private int getIndex(String index){ // convert any given key back to integer base 10 value
+        int sum = 0;
+        for(int x = 0; x < index.length(); x++){
+            int intVal = getIntegerValue(index.charAt(index.length() - (x + 1)));
+            sum += intVal * Math.pow(base, x);
+        }
+       return sum;
+    }
+
+    private int getIntegerValue(char c){ // get the integer index value for a given encode char key
+        for(int x = 0; x < key.length; x++){
+            if(key[x].equals(c)){
+                return x;
+            }
+        }
+        return -1; // throw runtime?
     }
 
     public String getNextFreeIndex(){
-        return null;
+        int curIndex = (wordCount + 1);
+        String res = "";
+        while(curIndex > 0){
+            res = key[ curIndex % base  ] + res;
+            curIndex /= base;
+        }
+        return res;
+    }
+
+
+    private void fillDictionary(){  // populate the dictionary at startup with hard coded words ( does not save custom words )
+        for(int x = 0; x < dictionaryString.length; x++){
+            dictionary.put(getNextFreeIndex(), dictionaryString[x]);
+            wordCount++;
+        }
+    }
+
+    public Map<String, String> getDictionary(){
+        return (Map<String, String>)this.dictionary.clone();
     }
 
     public int getDictionaryChecksum(){
-        return -1;
-    }
-
-    public Map<String, String> getDictionary(){ // hashmap
-        return this.dictionary;
+        return checksum;
     }
 
     public int getBase(){
@@ -78,4 +140,5 @@ public class CommunicationLoader {
     public int getEncodingSize(){
         return this.encodeSize;
     }
+
 }
--- f16cs350/project/loader/terrain/TerrainLoader.java
+++ f16cs350/project/loader/terrain/TerrainLoader.java
@@ -14,122 +14,123 @@ import java.util.*;
  **/
 
 public class TerrainLoader{
-	private File definition;    //  constructor initiate parse? Most likely not.
+	private File definition;
 
 	public TerrainLoader(File in){
 		this.definition = in;
 	}
 
-	public List<List<f16cs350.atc.datatype.CoordinatesWorld3D_ATC>> parse(){ //  THROW EXCEPTION??
-		int rlats = 1, rlons = 1, ralts = 1, nodes = 1, surfaces = 1;   // start at 1 so index matches
+	public List<List<f16cs350.atc.datatype.CoordinatesWorld3D_ATC>> parse() throws Exception{ //  THROW EXCEPTION?? -- no..
+		int rlats = 1, rlons = 1, ralts = 1, nodes = 1, surfaces = 1;
 		String rep = "";
 		Scanner scan = null;
         List<List<f16cs350.atc.datatype.CoordinatesWorld3D_ATC>> terrain = null;
 
         try {
 			scan = new Scanner(definition);
+			while(scan.hasNextLine()){
+				String temp = scan.nextLine();
+				rep += temp + "\n";
+				String[] tkn = temp.split(":| ");
 
-		while(scan.hasNextLine()){	// SCAN TO FIND ELEMENTS WHILE SAVING REPRESENTATION.
-			String temp = scan.nextLine();
-			rep += temp + "\n";	// scan file into string, no re-read.
-			String[] tkn = temp.split(":| ");
-
-			switch(tkn[0]){ // first token SHOULD BE TYPE!
-				case "RLAT":	rlats++;
-				break;
-				case "RLON":	rlons++;
-				break;
-				case "RALT":	ralts++;
-				break;
-				case "NODE":	nodes++;
-				break;
-				case "SURFACE":	surfaces++;
-				break;
-				case "TERRAIN":	break;
-				default:
+				switch(tkn[0]){
+					case "RLAT":	rlats++;
+						break;
+					case "RLON":	rlons++;
+						break;
+					case "RALT":	ralts++;
+						break;
+					case "NODE":	nodes++;
+						break;
+					case "SURFACE":	surfaces++;
+						break;
+					case "TERRAIN":	break;
+					default:
+				}
 			}
-		}
+			int[][] RLAT = new int[rlats][2], RLON = new int[rlons][2], NODES = new int[nodes][3];
+			double[] RLAT_sec = new double[rlats], RLON_sec = new double[rlons];
+			double[] RALT = new double[ralts];
+			ArrayList<Integer>[] SURFACES = (ArrayList<Integer>[]) new ArrayList[surfaces];
+			ArrayList<Integer> terrainPH = new ArrayList<>();
+			HashSet<Integer> nodeIndices = new HashSet<>();
+			scan = new Scanner(rep);
 
-		int[][] RLAT = new int[rlats][2], RLON = new int[rlons][2], NODES = new int[nodes][3];
-		double[] RLAT_sec = new double[rlats], RLON_sec = new double[rlons];
-		int[] RALT = new int[ralts];
-		ArrayList<Integer>[] SURFACES = (ArrayList<Integer>[]) new ArrayList[surfaces];
-        ArrayList<Integer> terrainPH = new ArrayList<>();
-		scan = new Scanner(rep);
 
-		while(scan.hasNextLine()){	// 0RLAT 1# 2#:3#:4# -- 0RALT 1# 2#  -- TYPE INDEX VAL:VAL:VAL
-			String temp = scan.nextLine();
-			String[] tkn = temp.split(":| "); // delimit [SPACE] || :
+			while(scan.hasNextLine()){	// 0RLAT 1# 2#:3#:4# -- 0RALT 1# 2#  -- [TYPE] [INDEX] [VAL]:[VAL]:[VAL]
+				String temp = scan.nextLine();
+				String[] tkn = temp.split(":| "); // delimit [SPACE] || :
 
-			switch(tkn[0]){ // prevent duplicates?
-				case "RLAT":
-					RLAT[Integer.parseInt(tkn[1])][0] = Integer.parseInt(tkn[2]);	//DEG
-					RLAT[Integer.parseInt(tkn[1])][1] = Integer.parseInt(tkn[3]);	//MIN
-					RLAT_sec[Integer.parseInt(tkn[1])] = Double.parseDouble(tkn[4]); //SEC
-				break;
-				case "RLON":
-					RLON[Integer.parseInt(tkn[1])][0] = Integer.parseInt(tkn[2]);
-					RLON[Integer.parseInt(tkn[1])][1] = Integer.parseInt(tkn[3]);
-					RLON_sec[Integer.parseInt(tkn[1])] = Double.parseDouble(tkn[4]);
-				break;
-				case "RALT":
-				    //if(true){ //RALT[Integer.parseInt(tkn[1])]) { -- how to check if the index is already there?
-                        RALT[Integer.parseInt(tkn[1])] = Integer.parseInt(tkn[2]);    // FEET
-                    //}else{
-                    //    throw new RuntimeException("Duplicate RALT index, failed to parse file.");
-                    //}
-				break;
-				case "NODE":
-					//if(NODES[Integer.parseInt(tkn[1])] == null) {	// PREVENT DUPLICATE ENTRY
-						NODES[Integer.parseInt(tkn[1])][0] = Integer.parseInt(tkn[2]);
-						NODES[Integer.parseInt(tkn[1])][1] = Integer.parseInt(tkn[3]);
-						NODES[Integer.parseInt(tkn[1])][2] = Integer.parseInt(tkn[4]);
-					//}else{
-					//	throw new RuntimeException("Duplicate NODE index, failed to parse file.");
-					//}
-				break;
-				case "SURFACE":	//3+
-					ArrayList<Integer> surfaceNodes = new ArrayList<>();
-					for(int x = 2; x < tkn.length; x++){
-						surfaceNodes.add(Integer.parseInt(tkn[x])); // each node index
-					}
-					SURFACES[Integer.parseInt(tkn[1])] = surfaceNodes;
-				break;
-				case "TERRAIN":
-                    for(int x = 1; x < tkn.length; x++){
-                        terrainPH.add(Integer.parseInt(tkn[x])); // each node index
-                    }
-				    break;
-				default:
+				switch(tkn[0]){
+					case "RLAT":
+						RLAT[Integer.parseInt(tkn[1])][0] = Integer.parseInt(tkn[2]);	//DEG
+						RLAT[Integer.parseInt(tkn[1])][1] = Integer.parseInt(tkn[3]);	//MIN
+						RLAT_sec[Integer.parseInt(tkn[1])] = Double.parseDouble(tkn[4]); //SEC
+						break;
+					case "RLON":
+						RLON[Integer.parseInt(tkn[1])][0] = Integer.parseInt(tkn[2]);
+						RLON[Integer.parseInt(tkn[1])][1] = Integer.parseInt(tkn[3]);
+						RLON_sec[Integer.parseInt(tkn[1])] = Double.parseDouble(tkn[4]);
+						break;
+					case "RALT":
+						RALT[Integer.parseInt(tkn[1])] = Double.parseDouble(tkn[2]);
+						break;
+					case "NODE":	// ENFORCE NO DUPLICATE NODE INDICES!!!!!
+						int index = Integer.parseInt(tkn[1]);
+						if(!nodeIndices.contains(index)) {
+							NODES[index][0] = Integer.parseInt(tkn[2]);
+							NODES[index][1] = Integer.parseInt(tkn[3]);
+							NODES[index][2] = Integer.parseInt(tkn[4]);
+							nodeIndices.add(index);
+						}else{	throw new RuntimeException("Duplicate node index!");	}
+						break;
+					case "SURFACE":	//3+
+						if(tkn.length < 5){	throw new RuntimeException("Not enough nodes for surface.");	}
+						ArrayList<Integer> surfaceNodes = new ArrayList<>();
+						for(int x = 2; x < tkn.length; x++){
+							surfaceNodes.add(Integer.parseInt(tkn[x]));
+						}
+						if(SURFACES[Integer.parseInt(tkn[1])] == null) {	SURFACES[Integer.parseInt(tkn[1])] = surfaceNodes;}
+						else{	throw new RuntimeException("DUPLICATE SURFACE!");	}
+						break;
+					case "TERRAIN":
+						for(int x = 1; x < tkn.length; x++){
+							terrainPH.add(Integer.parseInt(tkn[x])); // each node index
+						}
+						break;
+					default:
+				}
 			}
-		}
-        CoordinatesWorld3D_ATC[] COORDINATES = new CoordinatesWorld3D_ATC[nodes];
+			CoordinatesWorld3D_ATC[] COORDINATES = new CoordinatesWorld3D_ATC[nodes];
 
-        for(int z = 1; z < nodes; z++){ // create node 1 to N
-            COORDINATES[z] = new CoordinatesWorld3D_ATC(new Latitude_ATC( RLAT[z][0], RLAT[z][1], RLAT_sec[z]),
-                    new Longitude_ATC(RLON[z][0], RLON[z][1], RLON_sec[z]), new Altitude_ATC(RALT[z]));
-        }
+			for(int z = 1; z < nodes; z++){
+				COORDINATES[z] = new CoordinatesWorld3D_ATC(new Latitude_ATC( RLAT[z][0], RLAT[z][1], RLAT_sec[z]),
+						new Longitude_ATC(RLON[z][0], RLON[z][1], RLON_sec[z]), new Altitude_ATC(RALT[z]));
+			}
 
-        ArrayList<CoordinatesWorld3D_ATC>[] SURFACE_LISTS = (ArrayList<CoordinatesWorld3D_ATC>[]) new ArrayList[surfaces];
+			ArrayList<CoordinatesWorld3D_ATC>[] SURFACE_LISTS = (ArrayList<CoordinatesWorld3D_ATC>[]) new ArrayList[surfaces];
 
-        for(int x = 1; x < SURFACES.length; x++){ // start 1
-            ArrayList<CoordinatesWorld3D_ATC> surfaceCoordinates = new ArrayList<CoordinatesWorld3D_ATC>();
-            for(int y = 0; y < SURFACES[x].size(); y++){    // INDIVIDUAL NODE PLACEHOLDER FOR SURFACE CONSTRUCTION
-                surfaceCoordinates.add(COORDINATES[SURFACES[x].get(y)]); // add corresponding node according to placeholder
-            }
-            SURFACE_LISTS[x] = surfaceCoordinates; // add list to SURFACE OBJECT placeholder
-        }
+			for(int x = 1; x < SURFACES.length; x++){
+				ArrayList<CoordinatesWorld3D_ATC> surfaceCoordinates = new ArrayList<CoordinatesWorld3D_ATC>();
+				for(int y = 0; y < SURFACES[x].size(); y++){
+					surfaceCoordinates.add(COORDINATES[SURFACES[x].get(y)]);
+				}
+				if(SURFACE_LISTS[x] == null) {
+					SURFACE_LISTS[x] = surfaceCoordinates;
+				}else{
+					throw new RuntimeException("DUPLICATE SURFACE!");
+				}
 
-        terrain = new ArrayList<>();
+			}
 
-        for(int w = 0; w < SURFACE_LISTS.length - 1; w++){
-            terrain.add(SURFACE_LISTS[terrainPH.get(w)]);   // get place holder for surface, grab surface and add to list
-        }
+			terrain = new ArrayList<>();
 
+			for(int w = 0; w < SURFACE_LISTS.length - 1; w++){
+				terrain.add(SURFACE_LISTS[terrainPH.get(w)]);
+			}
 		}catch(Exception e){
-			throw new RuntimeException(e.getMessage()); // correct thing to do? should method throw?
+			throw new RuntimeException("File parse failed.");
 		}
-		System.out.println("TERRAIN SIZE" + terrain.size());
 
         return terrain;
 	}
