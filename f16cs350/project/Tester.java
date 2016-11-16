501e210f60b4357383ea4e5ecc31a2fff95a7fc5
cs350edwardsg <gedwards@eagles.ewu.edu>
2016-11-16 13:39:07 -0800

    Update Tester.java

--- f16cs350/project/Tester.java
+++ f16cs350/project/Tester.java
@@ -1,20 +1,25 @@
+//BETTER VERSION. Works with base 2 and base 36. 
+//Doesn't work with larger time values, yet. The parseInt function breaks there
+
 public class Tester
 {
-   private static int base = 10;
+   private static int base = 36;
    public static void main(String[] args)
    {
-      double time = 122.1001;
+      double time = 1.1;
       System.out.println(encodeTime(time));
-      time = 71.111;
+      /*time = 71.111;
       System.out.println(encodeTime(time));
       time = 7.111;
       System.out.println(encodeTime(time));
       time = 0.011;
-      System.out.println(encodeTime(time));
+      System.out.println(encodeTime(time));*/
    }
    
    public static String encodeTime(double time)//base 10
    {
+      long len = Math.round(Math.ceil(Math.log(9999999) / Math.log(base)));
+   
       long rounded = Math.round(time*10000);
    
       int newNum = Integer.parseInt((""+rounded),base);
@@ -22,8 +27,8 @@ public class Tester
       String zeros=""
       ,ret = ""+newNum;
       
-      int i,
-      g=(7-ret.length());
+      int i;
+      long g=(len-ret.length());
       
       for(i=0;i<g;i++)
          zeros += "0";
