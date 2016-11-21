c6581ada6e91e746090ebec7d320dcd5bf08fb31
cs350edwardsg <gedwards@eagles.ewu.edu>
2016-11-21 15:36:51 -0800

    Update Tester.java

--- f16cs350/project/Tester.java
+++ f16cs350/project/Tester.java
@@ -1,42 +1,57 @@
+import java.util.*;
+
 public class Tester
 {
-   private static int base;
+   private static int Base = 16,encodeSize =2;            
+            
    public static void main(String[] args)
    {
-      double time = 999.4321;
-      System.out.println("time = "+time);
+      System.out.println(encodeStatement(123.45,"dog one two three cleared to land runway three one left"));
+   }
+   
+   public static int getEncodingSize()
+   {
+      return encodeSize;
+   }
+   
+   public static int getBase()
+   {
+      return Base;
+   }
+   
+   
+   //encodeStatement(123.45,"dog one two three cleared to land runway three one left");
+   public static String encodeStatement(double time, String words)
+   {   
+      int newNum,i=0,len = getEncodingSize(),base = getBase();
+      String temp,phraseString = "",timeString = encodeTime(time);
+   
+      ArrayList<String> wordList = new ArrayList<String>();
       
-      base = 10;
-      System.out.println("base 10:\t"+encodeTime(time));
+      for (String retval: words.split(" ")) 
+         wordList.add(retval.toUpperCase());
+
+      while(!wordList.isEmpty())
+      {
+         temp = Integer.toString(turnIntoNumber(wordList.remove(0)),base).toUpperCase();
+         int g=(len-temp.length());
+         String zeros="";
+         for(i=0;i<g;i++){
+            zeros += "0";}
+         phraseString += (zeros+temp);
+      }
       
-      base = 2;
-      System.out.println("base 2:  \t"+encodeTime(time));
-      
-      base = 36;
-      System.out.println("base 36:\t"+encodeTime(time));
-      
-      
-      time = 0.001;
-      System.out.println("time = "+time);
-      
-      base = 10;
-      System.out.println("base 10:\t"+encodeTime(time));
-      
-      base = 2;
-      System.out.println("base 2:  \t"+encodeTime(time));
-      
-      base = 36;
-      System.out.println("base 36:\t"+encodeTime(time));
+      return timeString + phraseString;
    }
    
    public static String encodeTime(double time)//base 10
    {
-      long len = Math.round(Math.ceil(Math.log(9999999) / Math.log(base)));
+      long len = Math.round(Math.ceil(Math.log(9999999) / Math.log(getBase())));
       
       int newNum = Integer.parseInt(( "" + Math.round( time * 10000 )) , 10);
       //           ^convert long to int    ^convert double to long
    
-      String ret = Integer.toString(newNum,base).toUpperCase();
+      String ret = Integer.toString(newNum,getBase()).toUpperCase();
       //           ^convert to target base  
    
       int i;
@@ -47,6 +62,11 @@ public class Tester
          
       return zeros + ret;
    }//return at target base
+   
+   public static int turnIntoNumber(String thing)
+   {  
+      return 14;
+   }
 }
 
 
